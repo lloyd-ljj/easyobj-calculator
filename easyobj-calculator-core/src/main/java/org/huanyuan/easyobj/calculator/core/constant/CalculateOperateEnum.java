f5a543ca4ce56830fe6ae80b38fec231fef9f663
lianjunjie <925934794@qq.com>
2023-05-05 17:11:10 +0800

    feat: 新增相关逻辑计算和去重逻辑

---
+++ easyobj-calculator-core/src/main/java/org/huanyuan/easyobj/calculator/core/constant/CalculateOperateEnum.java
@@ -0,0 +1,16 @@
+package org.huanyuan.easyobj.calculator.core.constant;
+
+/**
+ * @Author lianjunjie
+ * @Description
+ * @Date 2023-04-18 17:11
+ */
+public enum CalculateOperateEnum {
+
+    UNION,
+
+    INTERSECTION,
+
+    DIFFERENCE;
+
+}
